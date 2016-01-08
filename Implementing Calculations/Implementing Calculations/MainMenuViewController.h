5e7f08fc86cbe47b66f9a256da0ef5129a390127
Dallas <matthews.dallas@gmail.com>
2016-01-07 20:26:08 -0500

    cleanup

---
+++ Implementing Calculations/Implementing Calculations/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Implementing Calculations
+//
+//  Created by Dallas S Matthews on 1/6/16.
+//  Copyright © 2016 Dallas S Matthews. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
